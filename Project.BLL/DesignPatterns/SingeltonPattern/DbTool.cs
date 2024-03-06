f2cea3acc4cf27eaeed9f697c82f4128cd89041b
Ahmet Terzioğlu <Yevmen63@gmail.com>
2024-03-06 21:33:14 +0300

    BLL Yuklendi

---
+++ Project.BLL/DesignPatterns/SingeltonPattern/DbTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.SingeltonPattern
+{
+    public class DbTool
+    {
+        DBTool() { }
+
+        static MyContext _dbInstance;
+
+        public static MyContext DbInstance
+        {
+            get
+            {
+                if (_dbInstance == null) _dbInstance = new MyContext();
+                return _dbInstance;
+            }
+        }
+    }
+}
